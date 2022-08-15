d673c211bd2d542f7ef5d131dc501870a95ad054
Neeraj <neerajrock54321@gmail.com>
2022-08-16 02:54:19 +0530

    init

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+       
+          Neeraj Verma
+        
+      </span>
+      <div className="iconContainer">
+        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+          <i className="fab fa-instagram-square fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
